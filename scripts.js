7a9f564abd7113cb9445000bbd8302bf58bd259a
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2023-11-28 16:49:53 -0300

    Set all the choices and compare to set a winner

--- scripts.js
+++ scripts.js
@@ -19,10 +19,34 @@ function getcomputerchoice() {
 }
 
 function buttonPressed() {
-    let input = prompt('Choose between rock, paper or scissors!');
+    let input = prompt('Choose between rock, paper or scissors:', "rock");
     let playerChoice = input.toLowerCase();
     let pcChoice = getcomputerchoice()
     console.log(pcChoice)
     console.log(playerChoice)
+    if (playerChoice == pcChoice) {
+        alert("It's a tie!")
+    }
+    else if (playerChoice == "rock" && pcChoice == "scissors") {
+        alert("The player wins!")
+    }
+    else if (playerChoice == "rock" && pcChoice == "paper") {
+        alert("The PC wins!")
+    }
+    else if (playerChoice == 'paper' && pcChoice == 'rock') {
+        alert("The player wins!")
+    }
+    else if (playerChoice == 'paper' && pcChoice == 'scissors') {
+        alert("The PC wins!")
+    }
+    else if (playerChoice == 'scissors' && pcChoice == 'paper') {
+        alert("The player wins!")
+    }
+    else if (playerChoice == 'scissors' && pcChoice == 'rock') {
+        alert("The PC wins!")
+    }
+    else {
+        alert('You may only type "rock", "paper" or "scissors". No typos allowed')
+    }
 }
 rockPaperScissorsButton.addEventListener("click", buttonPressed)
