9d2cdfc9f6dce0c536f490964a47209bc040547c
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2023-11-28 16:31:15 -0300

    Add initial implementation of player choice

--- scripts.js
+++ scripts.js
@@ -2,19 +2,27 @@ const rockPaperScissorsButton = document.getElementById("button")
 function getcomputerchoice() {
     let a = Math.random()
     if (a <= 0.33) {
-        let pcChoice = 'Rock';
-        alert(pcChoice);
+        let pcChoice = 'rock';
+        // alert(pcChoice);
         return pcChoice;
     }
     else if (a > 0.33 && a <= 0.66) {
-        let pcChoice = 'Paper';
-        alert(pcChoice);
+        let pcChoice = 'paper';
+        // alert(pcChoice);
         return pcChoice;
     }
     else {
-        let pcChoice = 'Scissors';
-        alert(pcChoice);
+        let pcChoice = 'scissors';
+        // alert(pcChoice);
         return pcChoice;
     }
 }
-rockPaperScissorsButton.addEventListener("click", getcomputerchoice)
+
+function buttonPressed() {
+    let input = prompt('Choose between rock, paper or scissors!');
+    let playerChoice = input.toLowerCase();
+    let pcChoice = getcomputerchoice()
+    console.log(pcChoice)
+    console.log(playerChoice)
+}
+rockPaperScissorsButton.addEventListener("click", buttonPressed)
