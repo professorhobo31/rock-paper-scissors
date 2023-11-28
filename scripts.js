c0c4986a6f900a231752d1ac4710bd13508c12c9
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2023-11-28 16:55:23 -0300

    Expand text alerts to show picks, remove test code

--- scripts.js
+++ scripts.js
@@ -3,23 +3,20 @@ function getcomputerchoice() {
     let a = Math.random()
     if (a <= 0.33) {
         let pcChoice = 'rock';
-        // alert(pcChoice);
         return pcChoice;
     }
     else if (a > 0.33 && a <= 0.66) {
         let pcChoice = 'paper';
-        // alert(pcChoice);
         return pcChoice;
     }
     else {
         let pcChoice = 'scissors';
-        // alert(pcChoice);
         return pcChoice;
     }
 }
 
 function buttonPressed() {
-    let input = prompt('Choose between rock, paper or scissors:', "rock");
+    let input = prompt('Choose between rock, paper or scissors:');
     let playerChoice = input.toLowerCase();
     let pcChoice = getcomputerchoice()
     console.log(pcChoice)
@@ -28,22 +25,22 @@ function buttonPressed() {
         alert("It's a tie!")
     }
     else if (playerChoice == "rock" && pcChoice == "scissors") {
-        alert("The player wins!")
+        alert("The player wins! Rock beats Scissors.")
     }
     else if (playerChoice == "rock" && pcChoice == "paper") {
-        alert("The PC wins!")
+        alert("The PC wins! Paper beats Rock")
     }
     else if (playerChoice == 'paper' && pcChoice == 'rock') {
-        alert("The player wins!")
+        alert("The player wins! Paper beats Rock")
     }
     else if (playerChoice == 'paper' && pcChoice == 'scissors') {
-        alert("The PC wins!")
+        alert("The PC wins! Scissors beat Paper")
     }
     else if (playerChoice == 'scissors' && pcChoice == 'paper') {
-        alert("The player wins!")
+        alert("The player wins! Scissors beat Paper")
     }
     else if (playerChoice == 'scissors' && pcChoice == 'rock') {
-        alert("The PC wins!")
+        alert("The PC wins! Rock beats Scissors")
     }
     else {
         alert('You may only type "rock", "paper" or "scissors". No typos allowed')
