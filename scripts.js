1a996639ac57873c4a5676625a64d97e63d1c0f6
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2023-11-29 16:14:12 -0300

    Complete 5 game variant, fix typo situation

--- scripts.js
+++ scripts.js
@@ -21,8 +21,8 @@ function buttonPressed() {
     let input = prompt('Choose between rock, paper or scissors:', 'rock');
     let playerChoice = input.toLowerCase();
     let pcChoice = getcomputerchoice()
-    // console.log(pcChoice)
-    // console.log(playerChoice)
+    console.log(pcChoice)
+    console.log(playerChoice)
     if (playerChoice == pcChoice) {
         alert("It's a tie. REMATCH!!!");
         return buttonPressed();
@@ -36,19 +36,24 @@ function buttonPressed() {
         return "pc";
     }
     else if (playerChoice == 'paper' && pcChoice == 'rock') {
-        alert("The player wins! Paper beats Rock")
+        alert("The player wins! Paper beats Rock");
+        return "player";
     }
     else if (playerChoice == 'paper' && pcChoice == 'scissors') {
-        alert("The PC wins! Scissors beat Paper")
+        alert("The PC wins! Scissors beat Paper");
+        return "pc";
     }
     else if (playerChoice == 'scissors' && pcChoice == 'paper') {
         alert("The player wins! Scissors beat Paper")
+        return "player";
     }
     else if (playerChoice == 'scissors' && pcChoice == 'rock') {
-        alert("The PC wins! Rock beats Scissors")
+        alert("The PC wins! Rock beats Scissors");
+        return "pc";
     }
     else {
-        alert('You may only type "rock", "paper" or "scissors". No typos allowed')
+        alert('You may only type "rock", "paper" or "scissors". No typos allowed');
+        return buttonPressed();
     }
 }
 rockPaperScissorsButton.addEventListener("click", buttonPressed)
@@ -64,11 +69,15 @@ function game() {
         else if (a == "pc") {
             pcScore++
         }
-        else if (a == "tie") {
-            i--
-        }
     }
     console.log(pcScore)
     console.log(playerScore)
+    
+    if (pcScore > playerScore) {
+        alert("The PC won the 5 round match!")
+    }
+    else if (playerScore > pcScore) {
+        alert("The player won the 5 round match!")
+    }
 }
 bestOfFiveButton.addEventListener("click", game)
