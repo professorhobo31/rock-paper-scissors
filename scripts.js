01462b87edc50063d3ba24cb1163df06fdd0670f
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-02-08 11:26:10 -0300

    Add basic functionality to buttons, edited game code

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ function singleRound(playerChoice) {
     console.log(playerChoice)
     if (playerChoice == pcChoice) {
         alert("It's a tie. REMATCH!!!");
-        // return singleRound();
+        return "";
     }
     else if (playerChoice == "rock" && pcChoice == "scissors") {
         alert("The player wins! Rock beats Scissors.");
@@ -58,44 +58,93 @@ function singleRound(playerChoice) {
 }
 // rockPaperScissorsButton.addEventListener("click", singleRound)
 
-function fiveRound() {
-    let pcScore = 0
-    let playerScore = 0
-    for (let i = 1; i <= 5; i++) {
-        let a = singleRound()
-        if (a == "player") {
-            playerScore++
-        }
-        else if (a == "pc") {
-            pcScore++
-        }
-    }
-    console.log(pcScore)
-    console.log(playerScore)
+// function fiveRound(playerChoice) {
+//     let pcScore = 0
+//     let playerScore = 0
+//     for (let i = 1; i <= 5; i++) {
+//         let a = singleRound(playerChoice)
+//         if (a == "player") {
+//             playerScore++
+//         }
+//         else if (a == "pc") {
+//             pcScore++
+//         }
+//     }
+//     console.log(pcScore)
+//     console.log(playerScore)
     
-    if (pcScore > playerScore) {
-        alert("The PC won the 5 round match!")
-    }
-    else if (playerScore > pcScore) {
-        alert("The player won the 5 round match!")
-    }
-}
+//     if (pcScore > playerScore) {
+//         alert("The PC won the 5 round match!")
+//     }
+//     else if (playerScore > pcScore) {
+//         alert("The player won the 5 round match!")
+//     }
+// }
 // bestOfFiveButton.addEventListener("click", fiveRound)
 
 const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
+let playerScore = 0
+let pcScore = 0
+let playerDisplay = document.querySelector('.player')
+let pcDisplay = document.querySelector('.AI')
 
 function rockChoice() {
-    singleRound('rock')
+    let result = singleRound('rock')
+    if (result == "player") {
+        playerScore++
+        console.log(playerScore)
+        console.log(pcScore)
+        playerDisplay.textContent = playerScore
+    }
+    else if (result == "pc") {
+        pcScore++
+        console.log(playerScore)
+        console.log(pcScore)
+        pcDisplay.textContent = pcScore
+    }
+    else {
+        return
+    }
 }
 
 function paperChoice() {
-    singleRound('paper')
+    let result = singleRound('paper')
+    if (result == "player") {
+        playerScore++
+        console.log(playerScore)
+        console.log(pcScore)
+        playerDisplay.textContent = playerScore
+    }
+    else if (result == "pc") {
+        pcScore++
+        console.log(playerScore)
+        console.log(pcScore)
+        pcDisplay.textContent = pcScore
+    }
+    else {
+        return
+    }
 }
 
 function scissorsChoice() {
-    singleRound('scissors')
+    let result = singleRound('scissors')
+    if (result == "player") {
+        playerScore++
+        console.log(playerScore)
+        console.log(pcScore)
+        playerDisplay.textContent = playerScore
+    }
+    else if (result == "pc") {
+        pcScore++
+        console.log(playerScore)
+        console.log(pcScore)
+        pcDisplay.textContent = pcScore
+    }
+    else {
+        return
+    }
 }
 
 rockBtn.addEventListener('click', rockChoice)
